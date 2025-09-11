891bc2b1d9d4f9c34769e2d5011be24d1e0775d6
simone5138 <79840250+simone5138@users.noreply.github.com>
2025-09-11 15:53:19 +0200

    Fix duplicate JPA repository definition

--- src/main/java/Neuroflow/backend/BackendApplication.java
+++ src/main/java/Neuroflow/backend/BackendApplication.java
@@ -6,10 +6,8 @@ import Neuroflow.backend.config.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
 @Import({DataBaseConfig.class, SecurityConfig.class})
 public class BackendApplication {
 
