6aa456622afff8422044af835afcfb3e2f3ba63b
simone5138 <degirolamosimone@gmail.com>
2025-09-11 17:40:10 +0200

    added dev flag without password crypt

--- src/main/java/Neuroflow/backend/config/SecurityConfig.java
+++ src/main/java/Neuroflow/backend/config/SecurityConfig.java
@@ -2,14 +2,23 @@ package Neuroflow.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
+    @Profile("!dev")
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Profile("dev")
+    public PasswordEncoder devPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
